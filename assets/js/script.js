e7a0eff3d4f28a99db514649109ce281a0f7f4a2
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-07 14:34:59 -0300

    function check

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,5 +29,14 @@ function mudarJogador(valor){
 }
 
 function checarVencedor(){
+    var quadrado1 = document.getElementById(1);
+    var quadrado2 = document.getElementById(2);
+    var quadrado3 = document.getElementById(3);
+    var quadrado4 = document.getElementById(4);
+    var quadrado5 = document.getElementById(5);
+    var quadrado6 = document.getElementById(6);
+    var quadrado7 = document.getElementById(7);
+    var quadrado8 = document.getElementById(8);
+    var quadrado9 = document.getElementById(9);
 
 }
