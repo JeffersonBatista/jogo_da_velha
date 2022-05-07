870842af91a1530abeb9d17b44ae38da708ac8ed
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-07 13:49:07 -0300

    initial rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 var jogador = null;
+var jogadorSelecionado = document.getElementById('jogador-selecionado');
+mudarJogador('X');
 
 function escolherQuadrado(id){
     // Teste para verificar o click - console.log(id);
     console.log(id);
+    var quadrado = document.getElementById(id);
+    if(quadrado.innerHTML !== '-'){
+        return;
+    }
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = 'white';
+    if(jogador === 'X'){
+        jogador = 'O';
+    }else{
+        jogador = 'X'
+    }
+    mudarJogador(jogador);
+}
+
+function mudarJogador(valor){
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
 }
